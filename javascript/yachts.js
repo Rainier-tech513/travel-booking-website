d49269a6718b4c26507a033fd460e86cde3de896
Rainier <gijsenrainier@gmail.com>
2025-08-01 19:12:47 +0200

    Verander de jachten in de onderste grid

--- javascript/yachts.js
+++ javascript/yachts.js
@@ -11,37 +11,37 @@ export const yachts = [
       },
       pricepweek: '2,600,000',
     }, {
-      image: "images/yacht-apho.jpg",
-      name: "Apho",
+      image: "images/charter-lady-s-yacht.jpg",
+      name: "Lady S",
       specs: {
-        length: "115.1m (377'7ft)",
-        guests: 16,
-        cabins: 8,
-        builtat: "Lurssen",
-        builtin: 2021
+        length: "93m (305'1ft)",
+        guests: 12,
+        cabins: 7,
+        builtat: "Feadship",
+        builtin: 2019
       },
-      pricepweek: '2,600,000',
+      pricepweek: '1,600,000',
     }, {
-      image: "images/yacht-apho.jpg",
-      name: "Apho",
+      image: "images/charter-sophia-yacht.jpg",
+      name: "Sophia",
       specs: {
-        length: "115.1m (377'7ft)",
-        guests: 16,
-        cabins: 8,
-        builtat: "Lurssen",
-        builtin: 2021
+        length: "96.6m (316'11ft)",
+        guests: 12,
+        cabins: 7,
+        builtat: "Feadship",
+        builtin: 2017
       },
-      pricepweek: '2,600,000',
+      pricepweek: '1,700,000',
     }, {
-      image: "images/yacht-apho.jpg",
-      name: "Apho",
+      image: "images/charter-man-of-steel-yacht.jpg",
+      name: "Man Of Steel",
       specs: {
-        length: "115.1m (377'7ft)",
-        guests: 16,
+        length: "72m (236'3ft)",
+        guests: 12,
         cabins: 8,
-        builtat: "Lurssen",
-        builtin: 2021
+        builtat: "CRN",
+        builtin: 2023
       },
-      pricepweek: '2,600,000',
+      pricepweek: '600,000',
     }];
    
